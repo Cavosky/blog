b4a041b5bfab285b5af301c423725a1feee90d8a
Lorenzo Cavagnaro <lorenzocavagnaro14@gmail.com>
2023-01-18 17:12:57 +0100

    modifiche

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
     </head>
-    <body class="bg-dark text-light">
+    <body class="bg-dark text-light z-0">
         <!--navbar-->
-        <nav class="navbar bg-dark navbar-warning border border-warning-subtle position-sticky z-1 top-0 end-0 w-100">
+        <nav class="navbar bg-dark navbar-warning border border-warning-subtle position-sticky z-2 top-0 end-0 w-100">
             <div class="container-fluid">     
                 <a class="navbar-brand text-warning fs-1">
                     <img src="provvisoria.jpg" class="rounded float-start border border-warning " width="50" height="50" alt="...">LibreComics
@@ -21,7 +21,7 @@
                 <input class="form-control m-2" type="search" placeholder="Naviga tra Articoli" aria-label="Cerca">
                 <button class="btn btn-outline-success" type="submit">Cerca</button>
                 <div class="text-end">
-                    <button type="button" class="btn btn-outline-light me-2 mb-1 ">Login</button>
+                    <button type="button" onclick="location.href='login.php'" class="btn btn-outline-light me-2 mb-1 ">Login</button>
                     <button type="button" class="btn btn-warning">Registrati</button>
                 </div>
             </form>
