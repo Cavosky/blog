76a210c5e94aca11e20a208e5f8615a3cd3dd775
Lorenzo Cavagnaro <lorenzocavagnaro14@gmail.com>
2023-05-04 17:46:13 +0200

    elimina commenti in successione

--- gestione/gestioneUtenti.js
+++ gestione/gestioneUtenti.js
@@ -7,33 +7,36 @@ $(document).ready(()=>{
 
         success: (output)=> {
                 $("#commenti").html(output);
-                $(".btn").click(()=>{
-                    $.ajax({
-                          url:"../connect.php",
-                          method:"POST",
-                          data:{eliminaCommento:document.getElementById('com').value},
-
-                            success: ()=> {     
-                                $.ajax({
-                                    url: "../connect.php",
-                                    method: "POST",
-                                    data: { commentiUtenteReload: document.getElementById('inv').value },
-
-                                        success:(res)=>{
-                                            $("#commenti").html(res);
-                                        }
-                                    
-                                })                      
-                            }
-                          
-                    });
-                });
+                ricorsiva();
             }
         });
     });
 });
 
+    function ricorsiva (){
+        $(".btn").click(() => {
+            $.ajax({
+                url: "../connect.php",
+                method: "POST",
+                data: { eliminaCommento: document.getElementById('com').value },
 
+                success: () => {
+                    $.ajax({
+                        url: "../connect.php",
+                        method: "POST",
+                        data: { commentiUtenteReload: document.getElementById('inv').value },
+
+                        success: (res) => {
+                            $("#commenti").html(res);
+                            ricorsiva();
+                        }
+
+                    })
+                }
+
+            });
+        });
+    }
 function passaggioNome(email) {
     document.getElementById('mod').value = document.getElementById(email).innerHTML;
     document.getElementById('inv').value = email;
